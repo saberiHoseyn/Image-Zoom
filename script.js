2111afe543d08b06775c976a55e961194ac6f5f8
saberi.hoseyn <saberi.hoseyn.hs@gmail.com>
2021-11-06 21:07:29 +0330

    Selects the image with the click event

--- script.js
+++ script.js
@@ -2,7 +2,33 @@
 
     let defineLibrary = ()=>({
         init : function(gallery){
-            console.log(gallery)
+            let container = document.querySelector(gallery);
+            
+            if(! container){
+                console.error("please create a container for your objZoom");
+                return;
+            }
+
+            let firstImg = container.querySelector(".small-previw");
+            let zoomedImg = container.querySelector(".zoom-img");
+
+            if(! firstImg){
+                console.error("Please add image with small-previw class to your gallery");
+                return;
+            };
+
+            if(! zoomedImg){
+                console.log("Please add a zoom-img tag");
+                return;
+            };
+
+            zoomedImg.style.backgroundImage = `url(${firstImg.src})`;
+
+            container.addEventListener("click" , function(event){
+                if(event.target.classList.contains("small-previw")){
+                    zoomedImg.style.backgroundImage = `url(${event.target.src})`;
+                }
+            })
         }
     });
 
